91e592b094543773b04e3dc98656968278881a61
sammynguyen0211 <sammynguyen0211@gmail.com>
2026-02-19 19:12:12 -0600

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import pandas as pd
 import datetime
@@ -20,28 +21,54 @@ def extract_features():
     
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['MSFT', 'IBM', 'GOOGL']
+    stk_tickers = ['AAPL', 'NVDA', 'TSLA','AMD','META' ]
     ccy_tickers = ['DEXJPUS', 'DEXUSUK']
-    idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
+    idx_tickers = ['SP500', 'NASDAQCOM', 'VIXCLS']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
     #stk_data = web.DataReader(stk_tickers, 'yahoo')
     ccy_data = web.DataReader(ccy_tickers, 'fred', start=START_DATE, end=END_DATE)
     idx_data = web.DataReader(idx_tickers, 'fred', start=START_DATE, end=END_DATE)
 
-    Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
-    Y.name = Y.name[-1]+'_Future'
-    
-    X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
-    X1.columns = X1.columns.droplevel()
-    X2 = np.log(ccy_data).diff(return_period)
-    X3 = np.log(idx_data).diff(return_period)
 
-    X = pd.concat([X1, X2, X3], axis=1)
+
+    return_period = 5
+    Y = stk_data['Adj Close']['NVDA'].pct_change(return_period).shift(-return_period)
+    Y.name = 'NVDA_Future_Return'
     
-    dataset = pd.concat([Y, X], axis=1).dropna().iloc[::return_period, :]
-    Y = dataset.loc[:, Y.name]
-    X = dataset.loc[:, X.columns]
+    # Features
+    X = pd.DataFrame({
+        'AAPL_return':    stk_data['Adj Close']['AAPL'].pct_change(),
+        'TSLA_return':    stk_data['Adj Close']['TSLA'].pct_change(),
+        'AMD_return':     stk_data['Adj Close']['AMD'].pct_change(),
+        'META_return':    stk_data['Adj Close']['META'].pct_change(),
+        'range':          (stk_data['High']['NVDA'] - stk_data['Low']['NVDA']) / stk_data['Close']['NVDA'],
+        'gap':            (stk_data['Open']['NVDA'] - stk_data['Close']['NVDA'].shift(1)) / stk_data['Close']['NVDA'].shift(1),
+        'momentum_10':    stk_data['Adj Close']['NVDA'] / stk_data['Adj Close']['NVDA'].shift(10) - 1,
+        'volatility_14':  stk_data['Adj Close']['NVDA'].pct_change().rolling(14).std(),
+    })
+    
+    # Combine and drop missing values
+    dataset = pd.concat([Y, X], axis=1).dropna()
+    
+
+    #Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
+    #Y.name = Y.name[-1]+'_Future'
+    
+    #X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
+    #X1.columns = X1.columns.droplevel()
+    #X2 = np.log(ccy_data).diff(return_period)
+    #X3 = np.log(idx_data).diff(return_period)
+
+    #X = pd.concat([X1, X2, X3], axis=1)
+    
+    #dataset = pd.concat([Y, X], axis=1).dropna().iloc[::return_period, :]
+
+    Y = dataset['NVDA_Future_Return']
+    X = dataset.drop(columns=['NVDA_Future_Return'])
+
+    #Y = dataset.loc[:, Y.name]
+    #X = dataset.loc[:, X.columns]
     dataset.index.name = 'Date'
     #dataset.to_csv(r"./test_data.csv")
     features = dataset.sort_index()
@@ -66,4 +93,5 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
+
 
