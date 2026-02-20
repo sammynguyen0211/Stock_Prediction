e4af77de1790080b431960c8441b5094d6041f39
sammynguyen0211 <sammynguyen0211@gmail.com>
2026-02-19 18:32:18 -0600

    Update StreamlitApp_Week_3.py

--- Portfolio/StreamlitApp_Week_3.py
+++ Portfolio/StreamlitApp_Week_3.py
@@ -59,11 +59,9 @@ MODEL_INFO = {
     "endpoint": aws_endpoint,
     "explainer": 'explainer.shap',
     "pipeline": 'finalized_model.tar.gz',
-    "keys": ['AAPL_return', 'TSLA_return', 'AMD_return', 'META_return', 'range',
-       'gap', 'momentum_10', 'volatility_14'],
+    "keys": ['AAPL_return', 'TSLA_return', 'range', 'gap', 'momentum_10', 'volatility_14'],
     "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} 
-               for k in ['AAPL_return', 'TSLA_return', 'AMD_return', 'META_return', 'range',
-       'gap', 'momentum_10', 'volatility_14']]
+               for k in ['AAPL_return', 'TSLA_return', 'range', 'gap', 'momentum_10', 'volatility_14']]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -154,6 +152,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
